2bb68789010d680bc0b2c9e4ce4d76a23c7ffff2
Luis <lugoluisdev@gmail.com>
2023-11-05 21:51:28 -0400

    inicio

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+        <div class="container">
+            <div class="leftContainer">
+                <h1>Sistema de <b>inicio de sesion</b> y <b>Registro de usuarios</b> </h1>
+            </div>
+            <div class="rightContainer">
+                <a href="login.php"><button>Iniciar sesion</button></a><br>
+                <a href="registration.php"><button>Registrarte</button></a>
+            </div>
+        </div>
+</body>
+</html>
